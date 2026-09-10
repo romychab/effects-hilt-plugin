7896ffd34f269e2f0c633a7a6bd90ecec71a503d
Roman Andrushchenko <rom.andrushchenko@gmail.com>
2026-09-10 04:00:49 +0300

    feat: make content optional in EffectProvider composable function

--- effects-core/compose/src/main/java/com/uandcode/effects/core/compose/EffectProvider.kt
+++ effects-core/compose/src/main/java/com/uandcode/effects/core/compose/EffectProvider.kt
@@ -29,10 +29,14 @@ public val LocalEffectScope: ProvidableCompositionLocal<EffectScope> =
     compositionLocalOf { RootEffectScopes.global }
 
 /**
- * Get an effect implementation of a type [T] within the
- * existing [EffectProvider] component.
+ * Get an effect implementation of a type [T] within the content block
+ * of the existing [EffectProvider] component.
  *
- * @throws IllegalStateException if this method is called not within [EffectProvider]
+ * Please note, the [EffectProvider] content block is mandatory here. Effects
+ * provided by a leaf `EffectProvider(...)` call without a content block are
+ * attached to their interfaces, but they are not visible for this function.
+ *
+ * @throws IllegalStateException if this method is called outside of the [EffectProvider] content block
  * @throws IllegalArgumentException if the specified effect of type [T] is not
  *                                  managed by the [EffectProvider] component
  */
@@ -86,14 +90,37 @@ public inline fun <reified T: Any> getEffect(): T {
  * }
  * ```
  *
+ * The content is optional. If you don't need to call [getEffect] within nested
+ * composables, you can use EffectProvider as a leaf component:
+ *
+ * ```
+ * @Composable
+ * fun Root() {
+ *     val myEffectImpl1 = remember { MyEffectImpl1() }
+ *     EffectProvider(myEffectImpl1)
+ *     MyApp()
+ * }
+ * ```
+ *
+ * Effects are attached to their interfaces in both cases. The difference is the
+ * visibility of effects for the [getEffect] function: it works only within the
+ * content block. In the example above, a `getEffect()` call inside `MyApp()`
+ * throws [IllegalStateException], because `MyApp()` is a sibling of the
+ * EffectProvider component and not a part of its content.
+ *
  * @param effects the list of effect implementations managed by this EffectProvider;
  *                please note, only instances of classes annotated with [EffectClass] annotation are allowed
+ * @param scope the [EffectScope] which holds effect interfaces managed by this EffectProvider;
+ *              by default, the scope provided by [LocalEffectScope] is used
+ * @param content the optional content of this EffectProvider; effects listed in [effects]
+ *                param are attached to their interfaces even if the content is omitted,
+ *                but [getEffect] can be called only within the content block
  */
 @Composable
 public fun EffectProvider(
     effects: ImmutableList<Any>,
     scope: EffectScope = LocalEffectScope.current,
-    content: @Composable () -> Unit,
+    content: (@Composable () -> Unit)? = null,
 ) {
     val currentComposeEffectNode = LocalComposeEffectNode.current
     val newComposeEffectNode = remember(effects) {
@@ -109,7 +136,7 @@ public fun EffectProvider(
             onStart = newComposeEffectNode::start,
             onStop = newComposeEffectNode::stop,
         )
-        content()
+        content?.invoke()
     }
 }
 
@@ -157,14 +184,37 @@ public fun EffectProvider(
  * }
  * ```
  *
+ * The content is optional. If you don't need to call [getEffect] within nested
+ * composables, you can use EffectProvider as a leaf component:
+ *
+ * ```
+ * @Composable
+ * fun Root() {
+ *     val myEffectImpl1 = remember { MyEffectImpl1() }
+ *     EffectProvider(myEffectImpl1)
+ *     MyApp()
+ * }
+ * ```
+ *
+ * Effects are attached to their interfaces in both cases. The difference is the
+ * visibility of effects for the [getEffect] function: it works only within the
+ * content block. In the example above, a `getEffect()` call inside `MyApp()`
+ * throws [IllegalStateException], because `MyApp()` is a sibling of the
+ * EffectProvider component and not a part of its content.
+ *
  * @param effects the list of effect implementations managed by this EffectProvider;
  *                please note, only instances of classes annotated with [EffectClass] annotation are allowed
+ * @param scope the [EffectScope] which holds effect interfaces managed by this EffectProvider;
+ *              by default, the scope provided by [LocalEffectScope] is used
+ * @param content the optional content of this EffectProvider; effects listed in [effects]
+ *                param are attached to their interfaces even if the content is omitted,
+ *                but [getEffect] can be called only within the content block
  */
 @Composable
 public fun EffectProvider(
     vararg effects: Any,
     scope: EffectScope = LocalEffectScope.current,
-    content: @Composable () -> Unit,
+    content: (@Composable () -> Unit)? = null,
 ) {
     val effectsList = effects.toImmutableList()
     EffectProvider(
--- effects-koin/compose/src/main/java/com/uandcode/effects/koin/compose/EffectProvider.kt
+++ effects-koin/compose/src/main/java/com/uandcode/effects/koin/compose/EffectProvider.kt
@@ -2,7 +2,6 @@ package com.uandcode.effects.koin.compose
 
 import androidx.compose.runtime.Composable
 import com.uandcode.effects.core.EffectScope
-import com.uandcode.effects.core.compose.EffectProvider
 import com.uandcode.effects.koin.annotations.KoinEffect
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
@@ -12,8 +11,12 @@ import com.uandcode.effects.core.compose.EffectProvider as CoreEffectProvider
 import com.uandcode.effects.core.compose.getEffect as getCoreEffect
 
 /**
- * Get an effect implementation of a type [T] within the
- * existing [EffectProvider] component.
+ * Get an effect implementation of a type [T] within the content block
+ * of the existing [EffectProvider] component.
+ *
+ * Please note, the [EffectProvider] content block is mandatory here. Effects
+ * provided by a leaf `EffectProvider(...)` call without a content block are
+ * attached to their interfaces, but they are not visible for this function.
  */
 @Composable
 public inline fun <reified T: Any> getEffect(): T {
@@ -64,14 +67,37 @@ public inline fun <reified T: Any> getEffect(): T {
  * }
  * ```
  *
+ * The content is optional. If you don't need to call [getEffect] within nested
+ * composables, you can use EffectProvider as a leaf component:
+ *
+ * ```
+ * @Composable
+ * fun Root() {
+ *     val myEffectImpl1 = remember { MyEffectImpl1() }
+ *     EffectProvider(myEffectImpl1)
+ *     MyApp()
+ * }
+ * ```
+ *
+ * Effects are attached to their interfaces in both cases. The difference is the
+ * visibility of effects for the [getEffect] function: it works only within the
+ * content block. In the example above, a `getEffect()` call inside `MyApp()`
+ * throws [IllegalStateException], because `MyApp()` is a sibling of the
+ * EffectProvider component and not a part of its content.
+ *
  * @param effects the list of effect implementations managed by this EffectProvider;
  *                please note, only instances of classes annotated with [KoinEffect] annotation are allowed
+ * @param scope the Koin [Scope] used for resolving the [EffectScope] which holds effect
+ *              interfaces managed by this EffectProvider; by default, the current Koin scope is used
+ * @param content the optional content of this EffectProvider; effects listed in [effects]
+ *                param are attached to their interfaces even if the content is omitted,
+ *                but [getEffect] can be called only within the content block
  */
 @Composable
 public fun EffectProvider(
     effects: ImmutableList<Any>,
     scope: Scope = currentKoinScope(),
-    content: @Composable () -> Unit,
+    content: (@Composable () -> Unit)? = null,
 ) {
     val effectScope = scope.get<EffectScope>()
     CoreEffectProvider(
@@ -125,12 +151,35 @@ public fun EffectProvider(
  * }
  * ```
  *
+ * The content is optional. If you don't need to call [getEffect] within nested
+ * composables, you can use EffectProvider as a leaf component:
+ *
+ * ```
+ * @Composable
+ * fun Root() {
+ *     val myEffectImpl1 = remember { MyEffectImpl1() }
+ *     EffectProvider(myEffectImpl1)
+ *     MyApp()
+ * }
+ * ```
+ *
+ * Effects are attached to their interfaces in both cases. The difference is the
+ * visibility of effects for the [getEffect] function: it works only within the
+ * content block. In the example above, a `getEffect()` call inside `MyApp()`
+ * throws [IllegalStateException], because `MyApp()` is a sibling of the
+ * EffectProvider component and not a part of its content.
+ *
  * @param effects the list of effect implementations managed by this EffectProvider;
  *                please note, only instances of classes annotated with [KoinEffect] annotation are allowed
+ * @param scope the Koin [Scope] used for resolving the [EffectScope] which holds effect
+ *              interfaces managed by this EffectProvider; by default, the current Koin scope is used
+ * @param content the optional content of this EffectProvider; effects listed in [effects]
+ *                param are attached to their interfaces even if the content is omitted,
+ *                but [getEffect] can be called only within the content block
  */
 @Composable
 public fun EffectProvider(
     vararg effects: Any,
     scope: Scope = currentKoinScope(),
-    content: @Composable () -> Unit,
+    content: (@Composable () -> Unit)? = null,
 ): Unit = EffectProvider(effects.toImmutableList(), scope, content)
